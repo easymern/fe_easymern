ceced69a1404576c4890f8df54b5091cbce3ef30
JayArghArgh <jayarghargh@gmail.com>
2022-04-23 21:22:38 +0930

    testing setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("/home")
+      .then(res => res.json())
+      .then(data => setData(data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
