56c907f514c33eb67d19d845ca274d212bfbd08e
JayArghArgh <jayarghargh@gmail.com>
2022-04-28 18:42:52 +0930

    add redirect to home after login

--- src/mod_login/components/Login.js
+++ src/mod_login/components/Login.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import {useForm} from 'react-hook-form';
+import { useNavigate } from "react-router-dom";
+
 import AuthService from "../../services/auth.service";
 
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+  // Use the forms state for the form.
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => {
-    console.log("data: ", data);
+  const history = useNavigate();
 
-    console.log("we're going to hit the API nnnnow")
-    AuthService.login(data.username, data.password)
+  const onSubmit = async data => {
+    setLoading(true);
+    await AuthService.login(data.username, data.password)
+      .then(setLoading(false))
+      .then(history("/"))
   }
 
 
@@ -23,7 +29,13 @@ const Login = () => {
           {errors.username && errors.username.type === 'required' && <span>Username required</span>}
           {/*{errors.username && errors.username.type === "maxLength" && <span>Max length exceeded</span> }*/}
           <input type="password" placeholder="password" {...register("password", {required: true, minLength: 3, maxLength: 16})} />
+          {errors.password && errors.password.type === 'required' && <span>Password required</span>}
           <input type="submit" />
+          <div className={"form-group"}>
+            {loading && (
+              <span className="spinner-border spinner-border-sm"></span>
+            )}
+          </div>
         </form>
       </div>
     </div>
